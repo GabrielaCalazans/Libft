d2a3a4577ea1a1beabc5ee12ab6d8dff8480b173
gacalaza <gacalaza@student.42sp.org.br>
2022-10-13 01:37:18 +0200

    corrigindo erro da isprint

--- ft_isprint.c
+++ ft_isprint.c
@@ -6,14 +6,14 @@
 /*   By: gacalaza <gacalaza@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/12 19:43:08 by gacalaza          #+#    #+#             */
-/*   Updated: 2022/09/12 19:43:10 by gacalaza         ###   ########.fr       */
+/*   Updated: 2022/10/13 01:36:16 by gacalaza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_isprint(int argument)
 {
-	if (argument < 32 || argument == 127)
-		return (0);
-	else
+	if (argument > 31 && argument < 127)
 		return (1);
+	else
+		return (0);
 }
